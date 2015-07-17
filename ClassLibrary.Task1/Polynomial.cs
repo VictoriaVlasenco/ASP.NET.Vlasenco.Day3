263f4323772894724c0be622f50737d769676330
Victoria Vlasenco <victoria.vlasenco@gmail.com>
2015-07-17 14:10:07 +0300

    Operations were changed. Tests

--- ClassLibrary.Task1/Polynomial.cs
+++ ClassLibrary.Task1/Polynomial.cs
@@ -15,15 +15,25 @@ namespace ClassLibrary.Task1
         { 
             get
             {
-                if (coefficients != null) return coefficients;
+                if (coefficients != null)
+                {
+                    var arrayToReturn = new double[coefficients.Length];
+                    coefficients.CopyTo(arrayToReturn,0);
+                    return arrayToReturn;
+                }
                 return new double[] { };
             }
         }
 
-        public Polynomial(params double[] coeffitients)
+        public double this[int i]
         {
-            CheckParams(coeffitients);
-            this.coefficients = coeffitients;
+            get { return coefficients[i]; }
+        }
+
+        public Polynomial(params double[] coefficients)
+        {
+            CheckParams(ref coefficients);
+            this.coefficients = coefficients;
         }
 
         public Polynomial(Polynomial other)
@@ -41,15 +51,20 @@ namespace ClassLibrary.Task1
         {
             return Equals(obj as Polynomial);
         }
-
         public override int GetHashCode()
         {
             return (coefficients.GetHashCode());
         }
 
-        public object Clone()
+        public Polynomial Clone()
         {
-            var p = (Polynomial)(this);
+            var p = new Polynomial(this);
+            return p;
+        }
+
+        object ICloneable.Clone()
+        {
+            var p = new Polynomial(this);
             return p;
         }
 
@@ -65,17 +80,9 @@ namespace ClassLibrary.Task1
             return !(leftOperand == rightOperand);
         }
 
-        public static Polynomial operator +(Polynomial leftPolynom, Polynomial rightPolynom)
+        public static Polynomial operator +(Polynomial left, Polynomial right)
         {
-            int minLen;
-            Polynomial res;
-            DefineLen(leftPolynom, rightPolynom, out minLen, out res);
-            
-            for (int i = 0; i < minLen; i++)
-            {
-                res.Coefficients[i] = leftPolynom.Coefficients[i] + rightPolynom.Coefficients[i];
-            }
-            return res;
+            return MakeOperation(left, right, (d, d1) => d + d1);
         }
 
         public Polynomial Add(Polynomial otherPolynom)
@@ -83,18 +90,9 @@ namespace ClassLibrary.Task1
             return this + otherPolynom;
         }
 
-        public static Polynomial operator -(Polynomial minuendPolynom, Polynomial subPolynom)
+        public static Polynomial operator -(Polynomial minuend, Polynomial sub)
         {
-            int minLen;
-            Polynomial res;
-            DefineLen(minuendPolynom, subPolynom, out minLen, out res);
-            
-
-            for (int i = 0; i < minLen; i++)
-            {
-                res.Coefficients[i] = minuendPolynom.Coefficients[i] - subPolynom.Coefficients[i];
-            }
-            return res;
+            return MakeOperation(minuend, sub, (d, d1) => d - d1);
         }
 
         public Polynomial Subtract(Polynomial otherPolynom)
@@ -104,12 +102,12 @@ namespace ClassLibrary.Task1
 
         public static Polynomial operator *(double number, Polynomial multPolynom)
         {
-            var res = (Polynomial)multPolynom.Clone();
-            for (int i = 0; i < multPolynom.Coefficients.Length; i++)
-            {
-                res.Coefficients[i] = number * multPolynom.Coefficients[i];
-            }
-            return res;
+            return MakeOperation(number, multPolynom, (d, d1) => d*d1);
+        }
+
+        public static Polynomial operator *(Polynomial multPolynom, double number)
+        {
+            return MakeOperation(number, multPolynom, (d, d1) => d*d1);
         }
 
         public Polynomial Multiply(double number)
@@ -121,42 +119,69 @@ namespace ClassLibrary.Task1
         {
             double res = 0;
             for (int i = 0; i < coefficients.Length; i++)
-            {
-                res += coefficients[i]*Math.Pow(x, i);
+            {   
+                res += Coefficients[i]*Math.Pow(x, i);
             }
             return res;
         }
 
-        private static void DefineLen(Polynomial leftOperand, Polynomial rightOperand,out int minLen, out Polynomial p)
+        public override string ToString()
         {
-            if (leftOperand.Coefficients.Length <= rightOperand.Coefficients.Length)
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Coefficients.Length; i++)
             {
-                minLen = leftOperand.Coefficients.Length;
-                p = (Polynomial)rightOperand.Clone();
+                sb.Append(Coefficients[i]);
+                sb.Append(' ');
             }
-            else
-            {
-                minLen = rightOperand.Coefficients.Length;
-                p = (Polynomial)leftOperand.Clone();
-            }
+            return sb.ToString();
         }
 
-        private void CheckParams(double[] coeffitients)
+        private static int DefineLen(Polynomial left, Polynomial right)
         {
-            if (coeffitients == null)
+            return left.Coefficients.Length >= right.Coefficients.Length ? left.Coefficients.Length : right.Coefficients.Length;
+        }
+
+        private static Polynomial MakeOperation(Polynomial left, Polynomial right, Func<double, double, double> toDoFunc)
+        {
+            int maxLen = DefineLen(left, right);
+            double[] array = left.Coefficients;
+            if (left.Coefficients.Length != maxLen)
+                Array.Resize(ref array, maxLen);
+            else
+                maxLen = right.Coefficients.Length;
+            for (int i = 0; i < maxLen; i++)
+            {
+                array[i] = toDoFunc(array[i], right.Coefficients[i]);
+            }
+            return new Polynomial(array);
+        }
+
+        private static Polynomial MakeOperation(double number, Polynomial polynomial, Func<double, double, double> toDoFunc)
+        {
+            double[] array = polynomial.Coefficients;
+            for (int i = 0; i < polynomial.Coefficients.Length; i++)
+            {
+                array[i] = toDoFunc(number, polynomial.Coefficients[i]);
+            }
+            return new Polynomial(array);
+        }
+
+        private void CheckParams(ref double[] coeffs)
+        {
+            if (coeffs == null)
                 throw new ArgumentNullException();
             int zeroCount = 0;
-            if (coeffitients.Length == 0)
+            if (coeffs.Length == 0)
                 throw new ArgumentException("Too few arguments to initialize");
-            for (int i = coeffitients.Length - 1; i >= 0; i--)
+            for (int i = coeffs.Length - 1; i >= 0; i--)
             {
-                if (Math.Abs(coefficients[i]) < TOLERANCE)
+                if (Math.Abs(coeffs[i]) < TOLERANCE)
                     zeroCount++;
                 else
                     break;
             }
-            Array.Copy(coeffitients, zeroCount, coeffitients, 0, coefficients.Length - zeroCount);
-            if (coeffitients.Length == 0)
+            Array.Resize(ref coeffs, coeffs.Length - zeroCount);
+            if (coeffs.Length == 0)
                 throw new ArgumentException("Too few arguments to initialize");
         }
         private bool ArrayEquals (double[] a2)
---
+++ PolynomialTest/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using ClassLibrary.Task1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PolynomialTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreationPolynomialCoeffs_0_1_2_3()
+        {
+            var p = new Polynomial(0,1,2,3,0,0);
+            Assert.AreEqual(new Polynomial(0,1,2,3), p);
+        }
+
+        [TestMethod]
+        public void InitializePolynomialByAnotherOne()
+        {
+            var p = new Polynomial(0, 1, 2, 3);
+            var p1 = new Polynomial(p);
+            Assert.AreEqual(p1, p);
+        }
+
+        [TestMethod]
+        public void Polynomial_1_2_3_Plus_AnotherOne_0_10_Expected_1_12_3()
+        {
+            var p = new Polynomial(1, 2, 3);
+            var p1 = new Polynomial(0, 10);
+            Polynomial res = p + p1;
+            Assert.AreEqual(res, new Polynomial(1, 12, 3));
+        }
+        [TestMethod]
+        public void Polynomial_0_1_0_0_Add_AnotherOne_1_10_2_Expected_1_11_2()
+        {
+            var p = new Polynomial(0, 1, 0, 0);
+            var p1 = new Polynomial(1, 10, 2);
+            Polynomial res = p1.Add(p);
+            Assert.AreEqual(res, new Polynomial(1, 11, 2));
+        }
+
+        [TestMethod]
+        public void Polynomial_1_2_3_Minus_AnotherOne_0_10_Expected_1_Minus8_3()
+        {
+            var p = new Polynomial(1, 2, 3);
+            var p1 = new Polynomial(0, 10);
+            Polynomial res = p - p1;
+            Assert.AreEqual(res, new Polynomial(1, -8, 3));
+        }
+        [TestMethod]
+        public void FromPolynomial_0_1_0_0_Subtract_AnotherOne_1_10_2_Expected_Minus1_Minus9_Minus2()
+        {
+            var p = new Polynomial(0, 1, 0, 0);
+            var p1 = new Polynomial(1, 10, 2);
+            Polynomial res = p.Subtract(p1);
+            Assert.AreEqual(res, new Polynomial(-1, -9, -2));
+        }
+
+        [TestMethod]
+        public void Number_5_MultiplyBy_Polynomial_0_1_10_0_Expected_0_5_50()
+        {
+            var p = new Polynomial(0, 1, 10, 0);
+            Polynomial res = 5*p;
+            Assert.AreEqual(res, new Polynomial(0, 5, 50));
+        }
+
+        [TestMethod]
+        public void Polynomial_0_1_10_0_MultiplyBy_5_Expected_0_5_50()
+        {
+            var p = new Polynomial(0, 1, 10, 0);
+            Polynomial res = p*5;
+            Assert.AreEqual(res, new Polynomial(0, 5, 50));
+        }
+
+        [TestMethod]
+        public void CountValue_X_IsEquals5_Polynomial_5_2_Expected_15()
+        {
+            var p = new Polynomial(5, 2);
+            double d = p.CountValue(5);
+            Assert.AreEqual(d, 15);
+        }
+
+        [TestMethod]
+        public void PolynomialEquals()
+        {
+            var p = new Polynomial(5, 2, 0, 0);
+            var p1 = new Polynomial(5, 2);
+            Assert.AreEqual(p1.Equals(p), true);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InitializePolynimialByNullArray_ExpectedException()
+        {
+            var p = new Polynomial(0, 0, 0);
+        }
+
+        [TestMethod]
+        public void TryToChangePolynom_ExpectedUnchangedValue()
+        {
+            var p = new Polynomial(5, 2, 0, 0);
+            p.Coefficients[0] = 10;
+            Assert.AreEqual(new Polynomial(5 ,2), p);
+        }
+
+    }
+}
